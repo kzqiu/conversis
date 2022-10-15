ed266b94a6615132132dc93cc72a93e1a3652510
kq6032 <kevin.z.q77@gmail.com>
2022-10-15 11:34:32 -0400

    Added processing to sentiment analysis output.

--- server/app.py
+++ server/app.py
@@ -5,10 +5,17 @@ from werkzeug.utils import secure_filename
 import os
 import time
 
+
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = ['mp3', 'm4a']
+
 token = open('api_token.txt')
 API_TOKEN = token.readline()
+
+SENTIMENT_ENUM = {'POSITIVE': 1, 
+                  'NEUTRAL': 0, 
+                  'NEGATIVE': -1}
+
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -24,52 +31,62 @@ def hello_world():
 def get_sentiments():
     f_name = request.files['file'].filename
 
-    if f_name != '': 
-        upload_speech(request.files['file'])
+    upload_speech(request.files['file'])
 
-        headers_0 = {'authorization': API_TOKEN}
-        response_0 = requests.post('https://api.assemblyai.com/v2/upload',
-                                headers=headers_0,
-                                data=read_file(os.path.join(UPLOAD_FOLDER, f_name)))
-        
-        # deletes file after being uploaded to AssemblyAI
-        os.remove(os.path.join(UPLOAD_FOLDER, f_name))
-
-        f_link = response_0.json()['upload_url']
-
-        endpoint = "https://api.assemblyai.com/v2/transcript"
-        json = {
-            "audio_url": f_link, # have audio url from some source
-            "sentiment_analysis": True
-        }
-        headers = {
-            "authorization": API_TOKEN,
-            "content-type": "application/json"
-        }
-
-        start_time = time.time()
-        elapsed_time = 0
-
-        response = requests.post(endpoint, json=json, headers=headers)
-
-        new_endpoint = "https://api.assemblyai.com/v2/transcript/" + response.json()['id']
-
-        while elapsed_time < 15:
-            time.sleep(0.5)
-
-            response = requests.get(new_endpoint, headers={'authorization': API_TOKEN})
-
-            status = response.json()['status']
-
-            if (status == 'completed' or status == 'error'):
-                break
-
-            elapsed_time = time.time() - start_time
-            
-        return response.json()
+    headers_0 = {'authorization': API_TOKEN}
+    response_0 = requests.post('https://api.assemblyai.com/v2/upload',
+                            headers=headers_0,
+                            data=read_file(os.path.join(UPLOAD_FOLDER, f_name)))
     
-    return {}
+    # deletes file after being uploaded to AssemblyAI
+    os.remove(os.path.join(UPLOAD_FOLDER, f_name))
 
+    f_link = response_0.json()['upload_url']
+
+    endpoint = "https://api.assemblyai.com/v2/transcript"
+    json = {
+        "audio_url": f_link, # have audio url from some source
+        "sentiment_analysis": True
+    }
+    headers = {
+        "authorization": API_TOKEN,
+        "content-type": "application/json"
+    }
+
+    start_time = time.time()
+    elapsed_time = 0
+
+    response = requests.post(endpoint, json=json, headers=headers)
+
+    new_endpoint = "https://api.assemblyai.com/v2/transcript/" + response.json()['id']
+
+    while elapsed_time < 15:
+        time.sleep(0.5)
+
+        response = requests.get(new_endpoint, headers={'authorization': API_TOKEN})
+
+        status = response.json()['status']
+
+        if (status == 'completed' or status == 'error'):
+            break
+
+        elapsed_time = time.time() - start_time
+        
+    return process_sentiment_json(response.json())
+
+def process_sentiment_json(json):
+    sentiments = {}
+    sentiments['sentiment_analysis_results'] = json['sentiment_analysis_results']
+    avg_sent = 0
+
+    for res in sentiments['sentiment_analysis_results']:
+        avg_sent += float(res['confidence']) * SENTIMENT_ENUM[res['sentiment']]
+    
+    sentiments['average'] = avg_sent / len(sentiments['sentiment_analysis_results'])
+
+    return sentiments
+        
+    
 def read_file(filename, chunk_size=5242880):
     with open(filename, 'rb') as _file:
         while True:
