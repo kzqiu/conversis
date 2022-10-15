3616d81f0051bb518a9bd5496dd283e8ce30e778
kq6032 <kevin.z.q77@gmail.com>
2022-10-15 17:38:21 -0400

    Completed speaker label consistency when using multiple files.

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A webapp built using React, Flask, and the AssemblyAPI to analyze a conversation between multiple people. Makes use of speaker labeling and sentiment analysis to rate the individual phrases and the overall interaction between them, displayed on the React-based frontend.
 
+Note: Aside from Python and Node dependencies, you must install ffmpeg on your machine in order for the server to work correctly! Additionally, you must have the correct file structure as shown below:
+
+    work in progress...
+
 Built during HackHarvard 2022: Control, Alt, Create!
 
 Contributors: Kevin Qiu, Nicholas Zhang, Qazi Ashikin
--- server/app.py
+++ server/app.py
@@ -8,6 +8,7 @@ from pydub import AudioSegment
 
 
 UPLOAD_FOLDER = './uploads'
+LABEL_SPLICES = './label_splices'
 ALLOWED_EXTENSIONS = ['mp3', 'm4a']
 
 token = open('api_token.txt')
@@ -32,34 +33,48 @@ def hello_world():
 def get_initial_sentiments():
     f_name = request.files['file'].filename
     upload_speech(request.files['file'])
-
-    # TODO: generate splicing of audio for labels in order
-    sentiments = get_sentiments_data(f_name)
-
+    sentiments = get_sentiments_data(f_name, delete_file=False)
     timestamps = {'A': [], 'B': []}
 
-    for s in sentiments['sentiment_analysis_results']:
-        if s['speaker'] in timestamps:
-            timestamps[s['speaker']].append((s['start'], s['end']))
-    
-    # Create new audio splice files
+    for cur in sentiments['sentiment_analysis_results']:
+        if cur['speaker'] in timestamps:
+            timestamps[cur['speaker']].append([cur['start'], cur['end']])
 
+    # Create new audio splice files
+    audio = AudioSegment.from_mp3(os.path.join(UPLOAD_FOLDER, f_name))
+    new_f = audio[timestamps['A'][0][0]:timestamps['A'][0][1]]
+
+    for i in range(1, min(len(timestamps['A']), 3)):
+        # In the future, we can determine if the end of the splice actually makes a difference (1 millisecond) in output
+        new_f = new_f.append(audio[timestamps['A'][i][0]:timestamps['A'][i][1]])
+    
+    for i in range(0, min(len(timestamps['B']), 3)):
+        # In the future, we can determine if the end of the splice actually makes a difference (1 millisecond) in output
+        new_f = new_f.append(audio[timestamps['B'][i][0]:timestamps['B'][i][1]])
+    
+    new_f.export(os.path.join(LABEL_SPLICES, 'labels.mp3'), format='mp3')
     os.remove(os.path.join(UPLOAD_FOLDER, f_name))
 
-    return sentiments
+    return jsonify(sentiments)
 
 @app.route("/sentiment", methods=['POST'])
 def get_sentiments():
     f_name = request.files['file'].filename
     upload_speech(request.files['file'])
 
-    # TODO: splice in audio for each label in desired order (A > B > C > etc)
+    # TODO: splice in audio for each label in desired order (A > B)
+    # remove the initial few phrases (for training)
 
+    prepend = AudioSegment.from_mp3(os.path.join(LABEL_SPLICES, 'labels.mp3'))
+    audio = AudioSegment.from_mp3(os.path.join(UPLOAD_FOLDER, f_name))
+    
+    aud_out = prepend + audio
+    aud_out.export(os.path.join(UPLOAD_FOLDER, f_name), format='mp3')
 
-    return get_sentiments_data(f_name)
+    return jsonify(get_sentiments_data(f_name, aud_start=len(prepend)))
 
 # Doc Page: https://www.assemblyai.com/docs/audio-intelligence#sentiment-analysis
-def get_sentiments_data(f_name, delete_file=True):
+def get_sentiments_data(f_name, delete_file=True, aud_start=0):
     headers_0 = {'authorization': API_TOKEN}
     response_0 = requests.post('https://api.assemblyai.com/v2/upload',
                             headers=headers_0,
@@ -89,7 +104,7 @@ def get_sentiments_data(f_name, delete_file=True):
 
     new_endpoint = "https://api.assemblyai.com/v2/transcript/" + response.json()['id']
 
-    while elapsed_time < 15:
+    while elapsed_time < 35:
         time.sleep(0.5)
 
         response = requests.get(new_endpoint, headers={'authorization': API_TOKEN})
@@ -101,19 +116,31 @@ def get_sentiments_data(f_name, delete_file=True):
 
         elapsed_time = time.time() - start_time
         
-    return jsonify(process_sentiment_json(response.json()))
+    return process_sentiment_labels(process_sentiment_json(response.json(), aud_start=aud_start))
 
-def process_sentiment_json(json):
+def process_sentiment_json(json, aud_start=0):
     sentiments = {}
     sentiments['sentiment_analysis_results'] = json['sentiment_analysis_results']
     avg_sent = 0
 
+    new_results = []
+
     for res in sentiments['sentiment_analysis_results']:
-        avg_sent += float(res['confidence']) * SENTIMENT_ENUM[res['sentiment']]
+        if res['start'] >= aud_start:
+            avg_sent += float(res['confidence']) * SENTIMENT_ENUM[res['sentiment']]
+            new_results.append(res)
     
     sentiments['average'] = avg_sent / len(sentiments['sentiment_analysis_results'])
+    sentiments['sentiment_analysis_results'] = new_results
 
     return sentiments
+
+def process_sentiment_labels(json):
+    if len(json['sentiment_analysis_results']) > 0 and json['sentiment_analysis_results'][0]['speaker'] == 'B':
+        for res in json['sentiment_analysis_results']:
+            res['speaker'] = 'B' if res['speaker'] == 'A' else 'A'
+            
+    return json
     
 def read_file(filename, chunk_size=5242880):
     with open(filename, 'rb') as _file:
