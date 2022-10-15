72beff5d6c60ed270270e784779e7c586804a5d9
kq6032 <kevin.z.q77@gmail.com>
2022-10-14 21:37:29 -0400

    Added basic request structure for labeling and sentiment analysis requests from AssemblyAI.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-#conversation-analysis
+# conversation-analysis
+
+A webapp built using React, Flask, and the AssemblyAPI to analyze a conversation between multiple people. Makes use of speaker labeling and sentiment analysis to rate the individual phrases and the overall interaction between them, displayed on the React-based frontend.
+
+Built during HackHarvard 2022: Control, Alt, Create!
+
+Contributors: Kevin Qiu, Nicholas Zhang, Qazi Ashikin, Ziheng Huang
--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request
 from flask_cors import CORS
 import requests
 
@@ -8,6 +8,42 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 @app.route("/", methods=['GET'])
 def hello_world():
     return "hello, world!"
+
+# Doc Page: https://www.assemblyai.com/docs/core-transcription#speaker-labels-speaker-diarization
+@app.route("/labels", methods=['POST'])
+def get_speech_labels():
+    post_data = request.get_json()
+    
+    endpoint = "https://api.assemblyai.com/v2/transcript"
+    json = {
+        "audio_url": "",
+        "speaker_labels": True
+    }
+    headers = {
+        "authorization": "YOUR-API-TOKEN",
+        "content-type": "application/json"
+    }
+    response = requests.post(endpoint, json=json, headers=headers)
+
+    return response.json()
+
+# Doc Page: https://www.assemblyai.com/docs/audio-intelligence#sentiment-analysis
+@app.route("/sentiment", methods=['POST'])
+def get_sentiment():
+    post_data = request.get_json()
+
+    endpoint = "https://api.assemblyai.com/v2/transcript"
+    json = {
+        "audio_url": "", # have audio url from some source
+        "sentiment_analysis": True
+    }
+    headers = {
+        "authorization": "YOUR-API-TOKEN",
+        "content-type": "application/json"
+    }
+    response = requests.post(endpoint, json=json, headers=headers)
+    
+    return response.json()
 
 if __name__ == "__main__":
     app.run()
